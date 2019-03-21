9bee6d4bf25ebe6129e3888583cafc325d80004d
casper_yue@163.com <casper_yue@163.com>
2019-03-21 14:14:50 +0800

    <update><add verify schema>

--- nameko_service/gateway/gateway/schemas.py
+++ nameko_service/gateway/gateway/schemas.py
@@ -1,13 +1,17 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, ValidationError
+from marshmallow.validate import Length, Equal
 
 
 class RegisterUserSchema(Schema):
-    username = fields.String(required=True)
-    password = fields.String(required=True)
-    confirm_password = fields.String(required=True)
-    sms_code = fields.String(required=True)
+    username = fields.String(required=True, validate=Length(6, 12))
+    password = fields.String(required=True, validate=Length(6, 12))
+    confirm_password = fields.String(required=True, validate=Length(6, 12))
+    sms_code = fields.String(required=True, validate=Length(equal=4))
 
 
 class LoginUserSchema(Schema):
-    username = fields.String(required=True)
-    password = fields.String(required=True)
+    username = fields.String(required=True, validate=Length(6, 12))
+    password = fields.String(required=True, validate=Length(6, 12))
+
+
+
